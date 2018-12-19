ce3b45f41da59ed9b48cbce0e86a4c9e32bfee54
mokimo <bannedjobs@gmail.com>
2018-12-19 13:47:37 +0100

    fix(issues): use eslint, use the right layout

--- includes/render_include.js
+++ includes/render_include.js
@@ -1,37 +1,54 @@
-const liSDK = require('@livingdocs/node-sdk')
+const liSDK = require("@livingdocs/node-sdk");
 
-module.exports = async function renderInclude (
-  liClient, livingdoc, layout, includeConfig, publication
+module.exports = async function renderInclude(
+  liClient,
+  livingdoc,
+  layout,
+  includeConfig,
+  publication
 ) {
-  const layoutConfig = includeConfig.layouts[layout]
-  const component = livingdoc.createComponent(layoutConfig.template)
+  const layoutConfig = includeConfig.layouts[layout];
+  const component = livingdoc.createComponent(layoutConfig.template);
 
-  const includeContent = getIncludeContent(layoutConfig.contentSpec, publication)
-  const enrichmentArgs = [layoutConfig.contentEnrichments, liClient, component, publication]
-  const enrichments = await getContentEnrichments(...enrichmentArgs)
+  const includeContent = getIncludeContent(
+    layoutConfig.contentSpec,
+    publication
+  );
+  const enrichmentArgs = [
+    layoutConfig.contentEnrichments,
+    liClient,
+    component,
+    publication
+  ];
+  const enrichments = await getContentEnrichments(...enrichmentArgs);
 
-  component.setContent({...includeContent, ...enrichments})
-  return liSDK.document.renderComponent(component)
+  component.setContent({ ...includeContent, ...enrichments });
+  return liSDK.document.renderComponent(component);
+};
+
+function getIncludeContent(contentSpec, publication) {
+  return Object.keys(contentSpec).reduce((accumulator, prop) => {
+    const contentPropExtractor = contentSpec[prop];
+    const accumulatedContent = {};
+    const value = contentPropExtractor(publication);
+    if (value) accumulatedContent[prop] = value;
+    return { ...accumulator, ...accumulatedContent };
+  }, {});
 }
 
-function getIncludeContent (contentSpec, publication) {
-  return Object.keys(contentSpec)
-    .reduce((accumulator, prop) => {
-      const contentPropExtractor = contentSpec[prop]
-      const accumulatedContent = {}
-      const value = contentPropExtractor(publication)
-      if (value) accumulatedContent[prop] = value
-      return {...accumulator, ...accumulatedContent}
-    }, {})
-}
-
-async function getContentEnrichments (contentEnrichments = [], liClient, component, publication) {
-  const pendingEnrichments = contentEnrichments
-    .map(contentEnrichment => contentEnrichment({liClient, component, publication}))
-  let enrichments = {}
+async function getContentEnrichments(
+  contentEnrichments = [],
+  liClient,
+  component,
+  publication
+) {
+  const pendingEnrichments = contentEnrichments.map(contentEnrichment =>
+    contentEnrichment({ liClient, component, publication })
+  );
+  let enrichments = {};
   for (const pendingEnrichment of pendingEnrichments) {
-    const resolvedContentEnrichment = await pendingEnrichment || {}
-    enrichments = {...enrichments, ...resolvedContentEnrichment}
+    const resolvedContentEnrichment = (await pendingEnrichment) || {};
+    enrichments = { ...enrichments, ...resolvedContentEnrichment };
   }
-  return enrichments
+  return enrichments;
 }
--- includes/teaser.js
+++ includes/teaser.js
@@ -1,53 +1,62 @@
-const renderInclude = require('./render_include')
+const renderInclude = require("./render_include");
 
-module.exports = async function resolveEmbedTeaserIncludes ({
+module.exports = async function resolveEmbedTeaserIncludes({
   livingdoc,
   liClient,
   includes,
   includeConfig
 }) {
-  const dataFetchTasks = startDataFetchTasks(includes, liClient)
+  const dataFetchTasks = startDataFetchTasks(includes, liClient);
   for (const task of dataFetchTasks) {
-    const {include, request} = task
-    const {params} = include.getContent()
+    const { include, request } = task;
+    const { params } = include.getContent();
 
-    const layout = params.layout
-    validateConfig(layout, includeConfig.layouts)
+    const layout = params.layout;
+    validateConfig(layout, includeConfig.layouts);
 
-    const publication = await request
-    if (!publication) throw new Error(`Article embed with id "${params.mediaId}" not found`)
+    const publication = await request;
+    if (!publication)
+      throw new Error(`Article embed with id "${params.mediaId}" not found`);
 
-    const renderArgs = [liClient, livingdoc, layout, includeConfig, publication]
+    const renderArgs = [
+      liClient,
+      livingdoc,
+      layout,
+      includeConfig,
+      publication
+    ];
 
-    const html = await renderInclude(...renderArgs)
+    const html = await renderInclude(...renderArgs);
 
     // exclude includes that werent found, and include the html of resolved includes
-    !publication.error && include.resolve(html)
+    !publication.error && include.resolve(html);
   }
-}
+};
 
-function startDataFetchTasks (includes, liClient) {
+function startDataFetchTasks(includes, liClient) {
   return includes
     .filter(include => {
-      const {params} = include.getContent()
-      return !!params.mediaId
+      const { params } = include.getContent();
+      return !!params.mediaId;
     })
     .map(include => {
-      const {params} = include.getContent()
+      const { params } = include.getContent();
 
-      const request = liClient.getPublication({documentId: params.mediaId})
-      return {include, request}
-    })
+      const request = liClient.getPublication({ documentId: params.mediaId });
+      return { include, request };
+    });
 }
 
-function validateConfig (layout, layoutsConfig) {
-  const layoutConfig = layoutsConfig[layout]
+function validateConfig(layout, layoutsConfig) {
+  const layoutConfig = layoutsConfig[layout];
   if (!layoutConfig.template) {
-    const msg = `Template component ("template") for layout "${layout}" not specified in layout configuration`
-    throw new Error(msg)
+    throw new Error(
+      `Template component ("template") for layout "${layout}" not specified in layout configuration`
+    );
   }
   if (!layoutConfig.contentSpec) {
-    const msg = `Content spec ("contentSpec") for layout "${layout}" not specified in layout configuration`
-    throw new Error(msg)
+    throw new Error(
+      `Content spec ("contentSpec") for layout "${layout}" not specified in layout configuration`
+    );
   }
 }
