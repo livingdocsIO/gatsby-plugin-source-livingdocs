74c7347fbc5ad7f1894ca61ad1a7ebb667679ced
mokimo <bannedjobs@gmail.com>
2019-02-14 16:06:07 +0100

    chore(eslint): fix issues

--- includes/config/index.js
+++ includes/config/index.js
@@ -1,42 +1,42 @@
-const contentExtractor = require("./content_extractor");
+const contentExtractor = require('./content_extractor')
 
 module.exports = {
   teaser: {
     layouts: {
-      "author-embed": {
-        template: "teaser-author-template",
+      'author-embed': {
+        template: 'teaser-author-template',
         contentSpec: {
           title: contentExtractor.authorName,
           link: contentExtractor.link,
           authorLink: contentExtractor.link,
           text: contentExtractor.profile,
-          image: contentExtractor.image({ crop: "1:1", target: "authorImage" })
+          image: contentExtractor.image({crop: '1:1', target: 'authorImage'})
         }
       },
-      "sidebar-embed": {
-        template: "teaser-sidebar-template",
+      'sidebar-embed': {
+        template: 'teaser-sidebar-template',
         contentSpec: {
           title: contentExtractor.title,
           link: contentExtractor.link,
           flag: contentExtractor.flag,
           date: contentExtractor.publishDate
         },
-        contentEnrichments: [require("./enrichments/author_ref")]
+        contentEnrichments: [require('./enrichments/author_ref')]
       },
       hero: {
-        template: "teaser-hero-template",
+        template: 'teaser-hero-template',
         contentSpec: {
           title: contentExtractor.title,
           link: contentExtractor.link,
           flag: contentExtractor.flag,
           text: contentExtractor.description,
           date: contentExtractor.publishDate,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/author_ref")]
+        contentEnrichments: [require('./enrichments/author_ref')]
       },
       card: {
-        template: "teaser-card-template",
+        template: 'teaser-card-template',
         contentSpec: {
           title: contentExtractor.title,
           link: contentExtractor.link,
@@ -44,12 +44,12 @@ module.exports = {
           flag: contentExtractor.flag,
           text: contentExtractor.description,
           date: contentExtractor.publishDate,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/author_ref")]
+        contentEnrichments: [require('./enrichments/author_ref')]
       },
-      "card-no-image": {
-        template: "teaser-card-no-image-template",
+      'card-no-image': {
+        template: 'teaser-card-no-image-template',
         contentSpec: {
           title: contentExtractor.title,
           link: contentExtractor.link,
@@ -57,61 +57,61 @@ module.exports = {
           text: contentExtractor.description,
           date: contentExtractor.publishDate
         },
-        contentEnrichments: [require("./enrichments/author_ref")]
+        contentEnrichments: [require('./enrichments/author_ref')]
       },
-      "card-numbered": {
-        template: "teaser-card-numbered-template",
+      'card-numbered': {
+        template: 'teaser-card-numbered-template',
         contentSpec: {
           title: contentExtractor.title,
           link: contentExtractor.link,
           flag: contentExtractor.flag,
           date: contentExtractor.publishDate
         },
-        contentEnrichments: [require("./enrichments/author_ref")]
+        contentEnrichments: [require('./enrichments/author_ref')]
       },
-      "card-author": {
-        template: "teaser-card-author-template",
+      'card-author': {
+        template: 'teaser-card-author-template',
         contentSpec: {
           link: contentExtractor.link,
           text: contentExtractor.title,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/author_teaser")]
+        contentEnrichments: [require('./enrichments/author_teaser')]
       },
       gallery: {
-        template: "teaser-gallery-template",
+        template: 'teaser-gallery-template',
         contentSpec: {
           title: contentExtractor.title,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/gallery_teaser")]
+        contentEnrichments: [require('./enrichments/gallery_teaser')]
       },
-      "gallery-hero": {
-        template: "teaser-gallery-hero-template",
+      'gallery-hero': {
+        template: 'teaser-gallery-hero-template',
         contentSpec: {
           title: contentExtractor.title,
           text: contentExtractor.description,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/gallery_teaser")]
+        contentEnrichments: [require('./enrichments/gallery_teaser')]
       },
       video: {
-        template: "teaser-video-template",
+        template: 'teaser-video-template',
         contentSpec: {
           title: contentExtractor.title,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/video_teaser")]
+        contentEnrichments: [require('./enrichments/video_teaser')]
       },
-      "video-hero": {
-        template: "teaser-video-hero-template",
+      'video-hero': {
+        template: 'teaser-video-hero-template',
         contentSpec: {
           title: contentExtractor.title,
           text: contentExtractor.description,
-          image: contentExtractor.image({ crop: "16:9" })
+          image: contentExtractor.image({crop: '16:9'})
         },
-        contentEnrichments: [require("./enrichments/video_teaser")]
+        contentEnrichments: [require('./enrichments/video_teaser')]
       }
     }
   }
-};
+}
